867a8e95325678d794509f7be3fc6197d42d141f
yoelvp <yoelvp73@gmail.com>
2026-03-27 18:38:46 -0500

    style: formatted code

--- src/main.c
+++ src/main.c
@@ -3,99 +3,100 @@
 #include <unistd.h>
 
 static void print_lines(int width) {
-  for (int i = 0; i < width; i++)
-    printf("─");
-  printf("\n");
+    for (int i = 0; i < width; i++)
+        printf("─");
+    printf("\n");
 }
 
 static void add_notes(const char *path) {
-  char buffer[200];
-  FILE *f = fopen(path, "a");
+    char buffer[200];
+    FILE *f = fopen(path, "a");
 
-  if (f == NULL) {
-    perror("Error add new note");
-    return;
-  }
+    if (f == NULL) {
+        perror("Error add new note");
+        return;
+    }
 
-  printf("Enter your note > ");
-  if (!fgets(buffer, sizeof(buffer), stdin)) {
-    perror("fgets");
+    printf("%p", fgets(buffer, sizeof(buffer), f));
+    printf("Enter your note > ");
+    if (!fgets(buffer, sizeof(buffer), stdin)) {
+        perror("fgets");
+        fclose(f);
+        return;
+    }
+    fprintf(f, "%s", buffer);
     fclose(f);
-    return;
-  }
-  fprintf(f, "%s", buffer);
-  fclose(f);
 }
 
 static void read_notes(const char *path) {
-  struct winsize w;
-  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1 || w.ws_col == 0)
-    w.ws_col = 80;
-  FILE *f = fopen(path, "r");
-  char buffer[200];
+    struct winsize w;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1 || w.ws_col == 0)
+        w.ws_col = 80;
+    FILE *f = fopen(path, "r");
+    char buffer[200];
 
-  if (f == NULL) {
-    perror("No notes yet\n");
-    return;
-  }
+    if (f == NULL) {
+        perror("No notes yet\n");
+        return;
+    }
 
-  print_lines(w.ws_col);
-  while (fgets(buffer, sizeof(buffer), f) != NULL) {
-    printf("%s", buffer);
-  }
-  print_lines(w.ws_col);
+    print_lines(w.ws_col);
+    while (fgets(buffer, sizeof(buffer), f) != NULL) {
+        printf("%s", buffer);
+    }
+    print_lines(w.ws_col);
 
-  fclose(f);
+    fclose(f);
 }
 
 int main() {
-  char option;
-  char buffer[200];
-  char cwd[1024];
-  char path[2048];
-  const char *filename = "NOTES";
+    char option;
+    char buffer[200];
+    char cwd[1024];
+    char path[2048];
+    const char *filename = "NOTES";
 
-  if (getcwd(cwd, sizeof(cwd)) == NULL) {
-    perror("getcwd");
-    return 1;
-  }
-
-  if (snprintf(path, sizeof(path), "%s/%s", cwd, filename) >= sizeof(path)) {
-    fprintf(stderr, "Path long\n");
-    return 1;
-  }
-
-  do {
-    printf("[a]dd - [r]ead [c]lear - [q]uit\n");
-    printf("> ");
-    fflush(stdout);
-    if (!fgets(buffer, sizeof(buffer), stdin)) {
-      printf("\nEOF reciv, exit...");
-      break;
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        perror("getcwd");
+        return 1;
     }
 
-    if (buffer[0] == '\n')
-      continue;
-    option = buffer[0];
-
-    switch (option) {
-    case 'a':
-      add_notes(path);
-      break;
-    case 'r':
-      read_notes(path);
-      break;
-    case 'c':
-      printf("\033[H\033[J");
-      fflush(stdout);
-      break;
-    case 'q':
-      break;
-    default:
-      printf("Invalid selected option.\n");
-      printf("\n");
+    if (snprintf(path, sizeof(path), "%s/%s", cwd, filename) >= sizeof(path)) {
+        fprintf(stderr, "Path long\n");
+        return 1;
     }
-  } while (option != 'q');
 
-  return 0;
+    do {
+        printf("[a]dd - [r]ead [c]lear - [q]uit\n");
+        printf("> ");
+        fflush(stdout);
+        if (!fgets(buffer, sizeof(buffer), stdin)) {
+            printf("\nEOF reciv, exit...");
+            break;
+        }
+
+        if (buffer[0] == '\n')
+            continue;
+        option = buffer[0];
+
+        switch (option) {
+        case 'a':
+            add_notes(path);
+            break;
+        case 'r':
+            read_notes(path);
+            break;
+        case 'c':
+            printf("\033[H\033[J");
+            fflush(stdout);
+            break;
+        case 'q':
+            break;
+        default:
+            printf("Invalid selected option.\n");
+            printf("\n");
+        }
+    } while (option != 'q');
+
+    return 0;
 }
